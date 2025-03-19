b61cb8da6224ae7b590b874333ce3ed4799849e0
mananmaheshwari11 <maheshwarimanan2211@gmail.com>
2025-03-19 11:15:24 +0530

    created flight management for project

---
+++ src/main/java/org/example/flights/Flight_Management_Application.java
@@ -0,0 +1,11 @@
+package org.example.flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Flight_Management_Application {
+    public static void main(String[] args) {
+            SpringApplication.run(Flight_Management_Application.class, args);
+        }
+    }
